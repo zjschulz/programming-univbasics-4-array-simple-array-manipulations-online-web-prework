50c2a4c67863c42aecad3a545dd4266a91be9b8c
Zachary Schulz <zjschulz@gmail.com>
2019-08-22 02:19:39 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,9 +2,6 @@ def using_push(colors_in_the_rainbow, next_color)
   colors_in_the_rainbow.push("violet")
 end
 
-def 
-
-
-a = [ "b", "c", "d" ]
-a.unshift("a")   #=> ["a", "b", "c", "d"]
-a.unshift(1, 2)  #=> [ 1, 2, "a", "b", "c", "d"]
+bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
+bouroughs_in_nyc.unshift("a")   #=> ["a", "b", "c", "d"]
+bouroughs_in_nyc.unshift(1, 2)  #=> [ 1, 2, "a", "b", "c", "d"]
