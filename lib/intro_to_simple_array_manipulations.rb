21de9256cfa93fc5a5b55853b332c0ce621f86f3
Zachary Schulz <zjschulz@gmail.com>
2019-08-22 02:19:06 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,3 +2,9 @@ def using_push(colors_in_the_rainbow, next_color)
   colors_in_the_rainbow.push("violet")
 end
 
+def 
+
+
+a = [ "b", "c", "d" ]
+a.unshift("a")   #=> ["a", "b", "c", "d"]
+a.unshift(1, 2)  #=> [ 1, 2, "a", "b", "c", "d"]
