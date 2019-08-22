babf9879352e346e405083be7f6e6381fce701b8
Zachary Schulz <zjschulz@gmail.com>
2019-08-22 01:56:37 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,4 +2,6 @@ def using_push(colors_in_the_rainbow, next_color)
   colors_in_the_rainbow.push("violet")
 end
 
-def using_unshift
+def using_unshift(bouroughs_in_nyc)
+  using_unshift.unshift()
+end
