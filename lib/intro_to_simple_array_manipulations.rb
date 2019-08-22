1d9f52e988242c0d8833fe4b85fba5b8c059a2ad
Zachary Schulz <zjschulz@gmail.com>
2019-08-22 02:37:31 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -22,6 +22,6 @@ def shift_with_args(ice_cream_brands)
   ice_cream_brands.shift(2)
 end
 
-def using_concat(@my_favorite_things, more_favs)
+def using_concat(my_favorite_things, more_favs)
   my_favorite_things.concat(more_favs)
 end
