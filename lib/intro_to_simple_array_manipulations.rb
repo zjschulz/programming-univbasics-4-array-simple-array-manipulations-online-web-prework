5e6e80f07a024d24bf890f372f2c5fd5c2b7e007
Zachary Schulz <zjschulz@gmail.com>
2019-08-22 02:51:13 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -30,6 +30,6 @@ def using_insert(list_of_programming_languages,another_language)
   list_of_programming_languages.insert(4,"Python")
 end
 
-def using_uniq
-  
+def using_uniq(haircuts)
+  haircuts.uniq
 end
