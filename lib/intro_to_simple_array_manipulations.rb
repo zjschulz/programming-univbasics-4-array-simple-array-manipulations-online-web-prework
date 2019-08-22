48d8bb4d31d04bc411013d69af8a5bc6133e05e1
Zachary Schulz <zjschulz@gmail.com>
2019-08-22 02:20:14 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,5 +3,4 @@ def using_push(colors_in_the_rainbow, next_color)
 end
 
 bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
-bouroughs_in_nyc.unshift("a")   #=> ["a", "b", "c", "d"]
-bouroughs_in_nyc.unshift(1, 2)  #=> [ 1, 2, "a", "b", "c", "d"]
+bouroughs_in_nyc.unshift
