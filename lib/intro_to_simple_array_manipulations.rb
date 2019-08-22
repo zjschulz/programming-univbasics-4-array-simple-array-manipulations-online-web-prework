23e86470db893a3ee934e83ac7628b0dc18dc162
Zachary Schulz <zjschulz@gmail.com>
2019-08-22 03:08:17 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -42,6 +42,6 @@ def using_delete(instructors,no_offense_steven)
   instructors.delete("Steven")
 end
 
-def using_delete_at(famous_robots)
+def using_delete_at(famous_robots, deleted_robot)
   famous_robots.delete_at(2)
 end
