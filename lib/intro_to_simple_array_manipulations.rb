ad2a2a4d86347142b8a94fc3997540a1f053d47f
Zachary Schulz <zjschulz@gmail.com>
2019-08-22 02:49:56 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -26,6 +26,6 @@ def using_concat(my_favorite_things, more_favs)
   my_favorite_things.concat(more_favs)
 end
 
-def using_insert(list_of_programming_languages)
+def using_insert(list_of_programming_languages,another_language)
   list_of_programming_languages.insert(4,"Python")
 end
