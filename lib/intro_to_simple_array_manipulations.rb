8c76187a82c980bf4ed5869bc1b982cc54e22f08
Zachary Schulz <zjschulz@gmail.com>
2019-08-22 03:07:01 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -38,7 +38,7 @@ def using_flatten(instruments)
   instruments.flatten
 end
 
-def using_delete(instructors,famous_robots)
+def using_delete(instructors,no_offense_Steven)
   instructors.delete("Steven")
 end
 
