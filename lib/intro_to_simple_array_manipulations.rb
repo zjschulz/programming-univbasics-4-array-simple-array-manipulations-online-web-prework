a799ccdc77c07d16721e418ef282ff550c4cf65b
Zachary Schulz <zjschulz@gmail.com>
2019-08-22 02:21:07 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,5 +2,6 @@ def using_push(colors_in_the_rainbow, next_color)
   colors_in_the_rainbow.push("violet")
 end
 
+def using_unshift
 bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
 bouroughs_in_nyc.unshift("Staten Island")
